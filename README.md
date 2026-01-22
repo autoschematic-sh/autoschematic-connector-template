22d2c5b801e52526188a3cc82ea48412e0e794ed
Peter Sherman <pfnsec@gmail.com>
2026-01-22 12:40:46 -0800

    core: upgrade to 0.12.0

---
+++ README.md
@@ -0,0 +1,3 @@
+## autoschematic-connector-template
+
+This repo is designed as a reusable template for creating a new connector in Rust. It should be kept up to date with the latest `autoschematic-core` version.
--- src/connector.rs
+++ src/connector.rs
@@ -5,8 +5,8 @@ use std::{
 
 use async_trait::async_trait;
 use autoschematic_core::{
-    connector::{Connector, ConnectorOutbox, FilterOutput, GetResourceOutput, OpExecOutput, OpPlanOutput},
-    diag::DiagnosticOutput,
+    connector::{Connector, ConnectorOutbox, FilterResponse, GetResourceResponse, OpExecResponse, PlanResponseElement},
+    diag::DiagnosticResponse,
     error::{AutoschematicError, AutoschematicErrorType},
 };
 
@@ -20,22 +20,49 @@ impl Connector for BaseConnector {
     where
         Self: Sized,
     {
+        // In new(), you'll create and return a default instance with whatever
+        // empty structures the connector needs.
+        // Even if the configuration is invalid, new() should never fail unless something is seriously messed up.
+        // Loading and validating configuration is for init(), not new()! If new() fails, the connector 
+        // won't even be alive to tell the client that the configuration is wrong.
         Ok(Arc::new(BaseConnector { prefix: prefix.into() }))
     }
 
     async fn init(&self) -> Result<(), anyhow::Error> {
+        // In init(), you'll load and validate prefix-wide configuration files if needed,
+        // for example, the AWS S3 Connector (autoschematic-connector-aws-s3) loads ./${prefix}/aws/config.ron 
+        // in addition to ./${prefix}/aws/s3/config.ron in order to load AWS and S3-specific config params.
+        // The Kubernetes Connector (autoschematic-connector-k8s) loads ./${prefix}/k8s/config.yaml to find its
+        // kubeconfig(s) and set max concurrency per cluster connection, for instance.
         Ok(())
     }
 
-    async fn filter(&self, addr: &Path) -> Result<FilterOutput, anyhow::Error> {
-        Ok(FilterOutput::None)
+    async fn filter(&self, addr: &Path) -> Result<FilterResponse, anyhow::Error> {
+        // In filter(), you'll define which files belong to your connector.
+        // We'll use an example to explain what we mean. 
+        // For the SnowflakeConnector, here's how we respond to various addresses. 
+        // "snowflake/warehouses/data_team.sql" => FilterResponse::Resource
+        // "snowflake/warehouses/dummy_file.txt" => FilterResponse::None
+        // "snowflake/databases/customer_db/database.sql" => FilterResponse::Resource
+        // "snowflake/databases/customer_db/primary/schema.sql" => FilterResponse::Resource
+        // "snowflake/databases/customer_db/primary/tables/customer_orgs.sql" => FilterResponse::Resource
+        // In other words, this address decoding logic is the main mechanism by which connectors describe an 
+        // ontology of nested objects.
+        // Note that addresses never include the prefix here; connectors run with their working directory
+        // at the root of the git repo, but they are informed of their prefix at new() and should save it if they need it (for loading configs etc).
+        // In other words, if the full path is "./${prefix}/${addr}", connectors are only passed "./${addr}" in operations like filter, get, plan, etc.
+        Ok(FilterResponse::None)
     }
 
     async fn list(&self, subpath: &Path) -> Result<Vec<PathBuf>, anyhow::Error> {
         Ok(Vec::new())
     }
 
-    async fn get(&self, addr: &Path) -> Result<Option<GetResourceOutput>, anyhow::Error> {
+    async fn get(&self, addr: &Path) -> Result<Option<GetResourceResponse>, anyhow::Error> {
+        // Suppose that we ran get() with the following path:
+        // "snowflake/databases/customer_db/primary/tables/customer_orgs.sql"
+        // We would use snowflake to query the database "customer_db" and in schema "primary", we'd get the full DDL description of table "customer_orgs"
+        // and spit it out as Some(get_resource_response!(...)).
         Ok(None)
     }
 
@@ -44,12 +71,12 @@ impl Connector for BaseConnector {
         addr: &Path,
         current: Option<Vec<u8>>,
         desired: Option<Vec<u8>>,
-    ) -> Result<Vec<OpPlanOutput>, anyhow::Error> {
+    ) -> Result<Vec<PlanResponseElement>, anyhow::Error> {
         Ok(Vec::new())
     }
 
-    async fn op_exec(&self, addr: &Path, op: &str) -> Result<OpExecOutput, anyhow::Error> {
-        Ok(OpExecOutput {
+    async fn op_exec(&self, addr: &Path, op: &str) -> Result<OpExecResponse, anyhow::Error> {
+        Ok(OpExecResponse {
             outputs: None,
             friendly_message: None,
         })
@@ -59,7 +86,7 @@ impl Connector for BaseConnector {
         Ok(a == b)
     }
 
-    async fn diag(&self, addr: &Path, a: &[u8]) -> Result<DiagnosticOutput, anyhow::Error> {
-        Ok(DiagnosticOutput { diagnostics: Vec::new() })
+    async fn diag(&self, addr: &Path, a: &[u8]) -> Result<Option<DiagnosticResponse>, anyhow::Error> {
+        Ok(None)
     }
 }
